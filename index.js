ab3ad6144c3990378a19810eeef4e3fc97080dc0
wayne <wayne71112@gmail.com>
2021-08-14 06:02:43 +0800

    修正index.js中的IP

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const qs = require("querystring");
 
 const port = process.env.PORT || 3000;
-const ip = "127.0.0.1";
+const ip = "https://powerful-garden-90736.herokuapp.com"
 
 const sendResponse = (filename, statusCode, response) => {
   fs.readFile(`./${filename}`, (error, data) => {
