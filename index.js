8b17ba7067e267f7541a60d3b3ab5e214ce0209c
wayne <wayne71112@gmail.com>
2021-08-14 05:49:22 +0800

    heroku上傳第二次嘗試

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const qs = require("querystring");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const ip = "127.0.0.1";
 
 const sendResponse = (filename, statusCode, response) => {
